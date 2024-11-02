4d938a26907775c9d264f7f0f4672b4f4ef73028
Jhoan Sebastian Velasco Velasquez <76527654+jhoanvv95@users.noreply.github.com>
2024-11-02 10:59:51 -0500

    Update reto_interes_simple.py

--- reto_interes_simple.py
+++ reto_interes_simple.py
@@ -13,13 +13,13 @@ el programa debe solicitar al usuario los montos de:
 '''
 
 
-# Funciión que calcular el valor a pagar
+# Función que calcula el valor a pagar
 def calcular_interes_simple(capital, tasa_interes, tiempo):
     interes = capital * tasa_interes * tiempo
     total_a_pagar = capital + interes
     return total_a_pagar
 
-# Entrada de datos
+# Entrada de datos por consola
 capital_inicial = float(input("Ingrese el capital inicial: "))
 tasa_interes_anual = float(input("Ingrese la tasa de interés anual (en porcentaje): ")) / 100
 tiempo = float(input("Ingrese el tiempo en años: "))
@@ -27,5 +27,5 @@ tiempo = float(input("Ingrese el tiempo en años: "))
 # Se invoca la funcion calcular_interes_simple para obtener el valor a pagar
 total = calcular_interes_simple(capital_inicial, tasa_interes_anual, tiempo)
 
-# Impresion del resultado
+# Impresion del resultado en pantalla
 print(f"El valor total a pagar después de {tiempo} años es: ${total}")
